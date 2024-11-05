a139d6a11bad05610fb2823be5fe5491dde1bb8d
kale1st <devmath@yandex.com>
2024-11-05 16:00:46 +0100

    service added

--- src/app/temperature-display/temperature-display.service.ts
+++ src/app/temperature-display/temperature-display.service.ts
@@ -1,22 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TemperatureDisplayComponent } from './temperature-display/temperature-display.component';
-import { FormsModule } from '@angular/forms';
+import { Injectable, signal } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, TemperatureDisplayComponent, FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+@Injectable({
+  providedIn: 'root',
 })
-export class AppComponent {
-  title = 'temperature_app';
-  // initial signals, these can be called from db
+export class TemperatureDisplayService {
+  // initial signals
   minTemperature = signal<number>(10);
   maxTemperature = signal<number>(35);
   targetTemperature = signal<number>(19);
 
+  constructor() {}
+
+  // Handler methods if needed for manual update from events
   updateMinTemperature(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
     const value = inputElement.valueAsNumber;
